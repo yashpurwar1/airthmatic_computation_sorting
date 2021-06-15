add19257c2837d044c891255c368f9ea44e83c7e
yashpurwar1 <yashpurwar.yashpurwar.com>
2021-06-15 20:25:30 +0530

    Code for taking input

---
+++ airthmatic_sorting.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -x
+echo "Welcome to Arithmetic sorting problem"
+
+read -p "Enter the value for a: " a
+read -p "Enter the value for b: " b
+read -p "Enter the value for c: " c
+
+echo "Entered values are a=$a, b=$b and c=$c"
